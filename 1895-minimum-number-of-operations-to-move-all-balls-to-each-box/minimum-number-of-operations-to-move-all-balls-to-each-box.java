2a9ef666d2d0cd008e915ca208b99f307e7330f8
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-01-06 12:32:48 +0530

    Time: 27 ms (54.34%) | Memory: 44.7 MB (77.78%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -2,17 +2,12 @@ class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
         int[] ans = new int[n];
-        HashSet<Integer> hs = new HashSet<>();
 
         for(int i = 0; i < n; i++) {
             if(boxes.charAt(i) == '1') {
-                hs.add(i);
-            }
-        }
-
-        for(int i = 0; i < n; i++) {
-            for(int index: hs) {
-                ans[i] += Math.abs(i-index);
+                for(int j = 0; j < n; j++) {
+                    ans[j] += Math.abs(j-i);
+                }
             }
         }
 
@@ -20,7 +15,6 @@ class Solution {
     }
 }
 /**
-BRUTE APP - 2 - HASHMAP
+BRUTE FORCE 
 TC = O(n)
-SC = O(n)
- */
+SC = O(1)*/
