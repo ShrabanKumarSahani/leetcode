184795f2f51090de01d116437121af7117b78398
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-01-06 12:33:19 +0530

    Time: 3 ms (94.79%) | Memory: 44.6 MB (86.28%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -2,19 +2,30 @@ class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
         int[] ans = new int[n];
+        int cummValue = 0, cummSum = 0;
 
-        for(int i = 0; i < n; i++) {
-            if(boxes.charAt(i) == '1') {
-                for(int j = 0; j < n; j++) {
-                    ans[j] += Math.abs(j-i);
-                }
-            }
+        // finding moves for all left balls to index i
+        for (int i = 0; i < n; i++) {
+            ans[i] = cummSum;
+            cummValue += boxes.charAt(i) == '1' ? 1 : 0;
+            cummSum += cummValue;
+        }
+
+        cummValue = 0;
+        cummSum = 0;
+
+        // for right balls moves to index i
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] += cummSum;
+            cummValue += boxes.charAt(i) == '0' ? 0 : 1;
+            cummSum += cummValue;
         }
 
         return ans;
     }
 }
 /**
-BRUTE FORCE 
-TC = O(n)
-SC = O(1)*/
+ * Prefix/Suffix Sum
+ * TC = O(n)
+ * SC = O(1)
+ */
