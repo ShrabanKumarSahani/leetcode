95f48ca6645ff6cf5f014659ed3163e6075856d1
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-01-07 15:42:31 +0530

    Time: 8 ms (6.83%) | Memory: 42.5 MB (16.91%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,24 +1,46 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        List<String> ans = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         int n = words.length;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(i == j) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || words[i].length() > words[j].length())
                     continue;
-                }
-                if(words[j].contains(words[i])) {
-                    ans.add(words[i]);
+
+                if (isSubString(words[i], words[j])) {
+                    res.add(words[i]);
                     break;
                 }
             }
         }
 
-        return ans;
+        return res;
+    }
+
+    public boolean isSubString(String sub, String word) {
+        int n = word.length();
+        int m = sub.length();
+
+        for (int j = 0; j < n; j++) {
+            boolean isMatch = true;
+            int k = j, i = 0;
+
+            while (i < m) {
+                if (k >= word.length() || word.charAt(k) != sub.charAt(i)) {
+                    isMatch = false;
+                    break;
+                }
+                k++;
+                i++;
+            }
+            if (isMatch)
+                return true;
+        }
+        return false;
     }
 }
-/**
-Brute Force
-TC = O(n*l ^ 2) => n = no of words, l = length of longest word
-SC = O(1) => ans<> need to be result */
+
+/*
+ * custom compare method
+ */
