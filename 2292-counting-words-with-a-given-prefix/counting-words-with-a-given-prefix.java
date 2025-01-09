2b3049e0361820b4bcce77c0df499a50f206c0f8
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-01-09 21:37:50 +0530

    Time: 4 ms (10.57%) | Memory: 45.8 MB (5.10%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,15 +1,56 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isEndOfWord;
+    int count;
+
+    TrieNode() {
+        children = new TrieNode[26];
+        isEndOfWord = false;
+        count = 0;
+    }
+}
+
+class Trie {
+    TrieNode root;
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(String word) {
+        TrieNode pCrawl = root;
+        for(char ch : word.toCharArray()) {
+            int index = ch  - 'a';
+            if(pCrawl.children[index] == null) 
+                pCrawl.children[index] = new TrieNode();
+            pCrawl = pCrawl.children[index];
+            pCrawl.count++;
+        }
+        pCrawl.isEndOfWord = true;
+    }
+
+    int searchPrefixCount(String pref) {
+        TrieNode pCrawl = root;
+        for(char ch : pref.toCharArray()) {
+            int index = ch - 'a';
+            if(pCrawl.children[index] == null) {
+                return 0;
+            }
+            pCrawl = pCrawl.children[index];
+        }
+        return pCrawl.count;
+    }
+}
+
 class Solution {
     public int prefixCount(String[] words, String pref) {
-        int count = 0;
-        for(String word: words) {
-            if(word.startsWith(pref)) {
-                count++;
-            }
+        Trie trie = new Trie();
+        for(String word : words) {
+            trie.insert(word);
         }
-        return count;
+        return trie.searchPrefixCount(pref);
     }
 }
 /**
-TC = O(n^m)
-SC = O(n^m)
+TC = O(no of words ^ avg length of word + length of prefix)
+SC = O(no of words ^ avg length of word)
  */
