2ef46d064f72bbc75db76e55e2ef42362a4b844e
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-02-05 23:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (39.72%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -1,36 +1,31 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
         int n = s1.length();
-        if(s1.equals(s2)) {
+        if (s1.equals(s2)) {
             return true;
         }
 
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
         int diff = 0;
+        int firstIndex = 0, secondIndex = 0;
 
-        for(int i = 0; i < n; i++) {
-            if(s1.charAt(i) != s2.charAt(i)) {
+        for (int i = 0; i < n; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
                 diff++;
-            }
 
-            if(diff > 2) {
-                return false;
-            }
-
-            freq1[s1.charAt(i) - 'a']++;
-            freq2[s2.charAt(i) - 'a']++;
-        }
-
-        for(int i = 0; i < 26; i++) {
-            if(freq1[i] != freq2[i]) {
-                return false;
+                if (diff > 2) {
+                    return false;
+                } else if (diff == 1) {
+                    firstIndex = i;
+                } else {
+                    secondIndex = i;
+                }
             }
         }
 
-        return true;
+        return s1.charAt(firstIndex) == s2.charAt(secondIndex) && s1.charAt(secondIndex) == s2.charAt(firstIndex);
     }
 }
 /**
-TC = O(n)
-SC = O(1) */
+ * TC = O(n)
+ * SC = O(1)
+ */
