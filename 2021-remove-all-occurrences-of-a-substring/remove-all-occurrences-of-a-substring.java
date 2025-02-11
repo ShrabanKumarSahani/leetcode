d4eafe104557536cd4751b98ee5502683f4940ac
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-02-11 23:18:21 +0530

    Time: 11 ms (12.17%) | Memory: 45.3 MB (5.62%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,15 +1,41 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        while(true) {
-            int index = s.indexOf(part);
-            if(index == -1) {   // no more occurences
-                break;
+        Stack<Character> st = new Stack<>();
+        int m = s.length();
+        int n = part.length();
+
+        for (int i = 0; i < m; i++) {
+            st.push(s.charAt(i));
+            if (st.size() >= n && check(n, part, st)) {     // if st size > part length and part is present then popping
+                for (int j = 0; j < n; j++) {
+                    st.pop();
+                }
             }
-            s = s.substring(0,index) + s.substring(index + part.length());
         }
-        return s;
+
+        StringBuilder str = new StringBuilder();
+        while(!st.isEmpty()) {
+            str.append(st.pop());
+        }
+
+        return str.reverse().toString();
+    }
+
+    private boolean check(int n, String part, Stack<Character> st) {
+        Stack<Character> tempStack = new Stack<>();
+        tempStack.addAll(st);           // copy stack
+
+        for(int idx = n-1; idx >= 0; idx--) {
+            if(tempStack.peek() != part.charAt(idx)) {  // not match with part
+                return false;
+            }
+            tempStack.pop();
+        }
+
+        return true;
     }
 }
 /**
-TC = O(s.length * part.length) = O(n^2)
-SC = O(1) */
+ * TC = O(s.length * part.length) = O(n^2)
+ * SC = O(s.length)
+ */
