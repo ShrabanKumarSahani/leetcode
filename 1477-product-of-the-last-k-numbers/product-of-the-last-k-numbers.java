2c0f96512e5c1ea4169c53d600f2de7b49072936
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-02-15 00:19:32 +0530

    Time: 16 ms (63.31%) | Memory: 68.7 MB (74.65%) - LeetSync

---
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -0,0 +1,22 @@
+class ProductOfNumbers {
+    ArrayList<Integer> list = new ArrayList<>();
+    public ProductOfNumbers() {
+        list.clear();
+    }
+    
+    public void add(int num) {
+        if(num==0){
+            list.clear();
+            return;
+        }
+        int prev = (list.size()==0)?1:list.get(list.size()-1);
+        list.add(prev * num);
+    }
+    
+    public int getProduct(int k) {
+        int s = list.size();
+        if(s<k) return 0;
+        else if(s == k) return list.get(s-1);
+        else return (list.get(s-1) / list.get(s-1-k));
+    }
+}
