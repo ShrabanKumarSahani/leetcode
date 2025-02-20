54d18f89cfa3ccde8f5e5cb21a036115ce195e6e
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-02-21 00:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (77.55%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,36 +1,14 @@
 class Solution {
-    StringBuilder ans;
     public String findDifferentBinaryString(String[] nums) {
         int n = nums.length;
-        HashSet<String> set = new HashSet<>();
-        for(String num : nums) {
-            set.add(num);
-        }
-        ans = new StringBuilder("");
-        backtrack(nums, n, set, ans);
-        return ans.toString();
-    }
-
-    public boolean backtrack(String nums[], int n, HashSet<String> set, StringBuilder ans) {
-        if(ans.length() == n) {
-            if(!set.contains(ans.toString())) {
-                return true;
-            }
-            return false;
-        }
-
-        for(char ch = '0'; ch <= '1'; ch++) {
+        StringBuilder ans = new StringBuilder("");
+        for(int i = 0; i < n; i++) {
+            char ch = (nums[i].charAt(i) == '0' ? '1' : '0');
             ans.append(ch);
-            if(backtrack(nums, n, set, ans)) {
-                return true;
-            }
-            ans.deleteCharAt(ans.length() - 1);
         }
-
-        return false;
+        return ans.toString();
     }
 }
 /**
-TC = O(2^n)
-SC = O(n)
-*/
+TC = O(n)
+SC = O(n) */
