de4e61b424b451f7dfe7a532983a79470f47ed34
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-03-03 22:59:14 +0530

    Time: 5 ms (85.39%) | Memory: 68.3 MB (24.11%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,26 +1,37 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
        int n = nums.length;
-       List<Integer> lessPivot = new ArrayList<>();
-       List<Integer> equalPivot = new ArrayList<>();
-       List<Integer> greaterPivot = new ArrayList<>();
-
+       
+       int countLess = 0;
+       int countEqual = 0;
+       
        for(int num : nums) {
         if(num < pivot) {
-            lessPivot.add(num);
+            countLess++;
         } else if(num == pivot) {
-            equalPivot.add(num);
-        } else {
-            greaterPivot.add(num);
+            countEqual++;
         }
        }
 
-       List<Integer> ans = new ArrayList<>();
-        ans.addAll(lessPivot);
-        ans.addAll(equalPivot);
-        ans.addAll(greaterPivot);
+       int i = 0;           // < pivot
+       int j = countLess;   // == pivot
+       int k = countLess + countEqual; // > pivot
+       int[] ans = new int[n];
 
-        return ans.stream().mapToInt(i -> i).toArray();
+        for(int num : nums) {
+            if(num < pivot) {
+                ans[i] = num;
+                i++;
+            } else if(num == pivot) {
+                ans[j] = num;
+                j++;
+            } else {
+                ans[k] = num;
+                k++;
+            }
+        }
+
+        return ans;
     }
 }
 /**
