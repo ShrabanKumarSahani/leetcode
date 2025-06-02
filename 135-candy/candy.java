fd850492d3ee2e6d36dabc7c236f11e5ab8cf495
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-06-02 14:43:58 +0530

    Time: 3 ms (84.79%) | Memory: 45.8 MB (70.42%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,30 +1,26 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-
-        int[] L2R = new int[n];
-        int[] R2L = new int[n];
-
-        Arrays.fill(L2R, 1);
-        Arrays.fill(R2L, 1);
+        int[] count = new int[n];
+        Arrays.fill(count, 1);
 
         // comparing with left neighbour
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                L2R[i] = Math.max(L2R[i], L2R[i - 1] + 1);
+                count[i] = Math.max(count[i], count[i - 1] + 1);
             }
         }
 
         // comparing with right neighbour
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
-                R2L[i] = Math.max(R2L[i], R2L[i + 1] + 1);
+                count[i] = Math.max(count[i], count[i + 1] + 1);
             }
         }
 
         int ans = 0;
         for(int i = 0; i < n; i++) {
-            ans += Math.max(L2R[i], R2L[i]);
+            ans += count[i];
         }
 
         return ans;
@@ -32,4 +28,4 @@ class Solution {
 }
 /**
 TC = O(n)
-SC = O(2*n) */
+SC = O(n) */
