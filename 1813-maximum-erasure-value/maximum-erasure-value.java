c5d9903d3cde80577f7a11ae10716a4ba1eeef5f
Shraban Sahani <137869077+ShrabanKumarSahani@users.noreply.github.com>
2025-07-22 18:13:16 +0530

    Time: 56 ms (30.22%) | Memory: 61 MB (34.68%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.java
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -1,22 +1,23 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        Map<Integer, Integer> hm = new HashMap<>();
-        int i = 0, j = 0, curr_sum = 0, result = 0;
-        int n = nums.length;
-        while (j < n) {
-            hm.put(nums[j], hm.getOrDefault(nums[j], 0) + 1);
-            curr_sum += nums[j];
-            while (hm.get(nums[j]) > 1) {
-                hm.put(nums[i], hm.getOrDefault(nums[i], 0) - 1);
-                curr_sum -= nums[i];
-                i++;
+        Set<Integer> st = new HashSet<>();
+        int i = 0, j = 0, sum = 0, ans = 0;
+
+        while(i < nums.length) {
+            if(!st.contains(nums[i])) {
+                sum += nums[i];
+                ans = Math.max(sum, ans);
+                st.add(nums[i++]);
+            } else {
+                sum -= nums[j];
+                st.remove(nums[j++]);
             }
-            result = Math.max(result, curr_sum);
-            j++;
         }
-        return result;
+
+        return ans;
     }
-    /**
-    TC = O(n)
-    SC = O(n) */
 }
+/**
+TC = O(n)
+SC = O(n)
+*/
